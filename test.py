2113e533db6cb49cf8c0f9a856aa32714215cdaf
Lucy Felix <felix.lucy@wmsu.edu.ph>
2024-06-30 14:17:11 +0800

    v0.06

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import pandas as pd
-import plotly.express as px
 import folium
 from folium.plugins import HeatMap
 
-df=pd.read_csv('robbery.csv')
+df=pd.read_csv('dumpsites.csv')
 print(df.head())
+df.dropna(subset=['Population Density'],axis=0,inplace=True)
 
-fig=px.density_mapbox(df,lat='Latitude',lon='Longitude', z='Beat',radius=20,
-                      center=dict(lat=df.Latitude.mean(),lon=df.Longitude.mean()),
-                      zoom=4,mapbox_style='open-streetmap', height=900)
-fig.show()
+m=folium.Map(location=[df.Lat.mean(),df.Lon.mean()], zoom_start=6, control_scale=True)
+map_values=df[['Lat','Lon', 'Population Density']]
+data=map_values.values.tolist()
+hm=HeatMap(data, gradient={0.4: 'blue', 0.65: 'lime', 1: 'red'},min_opacity=0.05,max_opacity=9,radius=25).add_to(m)
+m.show_in_browser()
+
+
